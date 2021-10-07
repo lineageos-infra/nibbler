e0ead58a460118821a26520b7329a607bc9f0087
LuK1337 <priv.luk@gmail.com>
2021-10-07 20:43:40 +0200

    Handle multiple gerrit change URLs

--- cogs/gerrit.py
+++ cogs/gerrit.py
@@ -17,10 +17,10 @@ class Gerrit(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        regex = ".*https://review\.lineageos\.org/(?:(?:#\/)?c\/(?:LineageOS\/[a-zA-Z_0-9\-]*\/\+\/)?)?(\d+).*"
-        if match := re.match(regex, message.content):
+        regex = "https://review\.lineageos\.org/(?:(?:#\/)?c\/(?:LineageOS\/[a-zA-Z_0-9\-]*\/\+\/)?)?(\d+)"
+        for match in set(re.findall(regex, message.content)):
             req = requests.get(
-                f"https://review.lineageos.org/changes/{match[1]}?o=DETAILED_ACCOUNTS&o=DETAILED_ACCOUNTS&o=CURRENT_REVISION&o=CURRENT_COMMIT"
+                f"https://review.lineageos.org/changes/{match}?o=DETAILED_ACCOUNTS&o=DETAILED_ACCOUNTS&o=CURRENT_REVISION&o=CURRENT_COMMIT"
             )
             change = json.loads(req.text[4:])
             embed = discord.Embed(
