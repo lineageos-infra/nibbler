033939dab2418fea46559b941fded4f09230db5d
Tom Powell <tom@powell.io>
2021-10-08 21:42:03 -0700

    Move 'hardware' section to 'private', add/kick/leave support, move to group

--- cogs/roles.py
+++ cogs/roles.py
@@ -1,6 +1,7 @@
 from discord.ext import tasks, commands
 
 import discord
+from discord.ext.commands.errors import PrivateMessageOnly
 
 
 class Roles(commands.Cog):
@@ -142,30 +143,81 @@ class Roles(commands.Cog):
                 if not role in user.roles:
                     await user.add_roles(role)
 
-    @commands.command(help="Create a channel under 'hardware' for a device/oem")
+    @commands.group(help="commands related to hardware private channels")
+    async def private(self, ctx):
+        pass
+
+    @private.command(help="Create a channel under 'private'")
     @commands.has_role("Project Director")
-    async def devicechannel(self, ctx, device):
-        device_role = discord.utils.get(ctx.guild.roles, name=device)
+    async def create(self, ctx, name):
+        role = discord.utils.get(ctx.guild.roles, name=name)
 
-        category = discord.utils.get(ctx.guild.categories, name='hardware')
-        channel = discord.utils.get(ctx.guild.channels, name=device)
+        category = discord.utils.get(ctx.guild.categories, name='private')
+        channel = discord.utils.get(ctx.guild.channels, name=name)
 
-        if not device_role:
-            device_role = await ctx.guild.create_role(name=device)
+        if not role:
+            role = await ctx.guild.create_role(name=name)
 
         overwrites = {
             ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
             ctx.guild.me: discord.PermissionOverwrite(read_messages=True),
-            device_role: discord.PermissionOverwrite(read_messages=True)
+            role: discord.PermissionOverwrite(read_messages=True)
         }
         if not category:
-            category = await ctx.guild.create_category_channel(name='hardware', overwrites=overwrites)
+            category = await ctx.guild.create_category_channel(name='private', overwrites=overwrites)
         else:
-            await category.set_permissions(device_role, read_messages=True)
+            await category.set_permissions(role, read_messages=True)
 
         if not channel or channel.category != category:
-            channel = await ctx.guild.create_text_channel(name=device, category=category, overwrites=overwrites)
+            channel = await ctx.guild.create_text_channel(name=name, category=category, overwrites=overwrites)
 
+        await ctx.message.add_reaction("👍")
+
+    @private.command(help="Add a user to the current room. Note: user is in the form nick#1234")
+    async def add(self, ctx, user):
+        if not discord.utils.get(ctx.author.roles, name=ctx.channel.name):
+            await ctx.reply("You don't have permission to do this.")
+            return
+        if not '#' in user:
+            await ctx.reply("Since this channel's private you'll need to invite them as user#1234")
+            return
+        name, discriminator = user.split("#")
+        who = discord.utils.get(ctx.guild.members, name=name, discriminator=discriminator)
+        if not who:
+            await ctx.reply("This user doesn't exist in this server")
+            return
+        role = discord.utils.get(ctx.guild.roles, name=ctx.channel.name)
+        if not role:
+            await ctx.reply("This isn't a private enough channel!")
+            return
+        await who.add_roles(role)
+        await ctx.message.add_reaction("👍")
+
+    @private.command(help="kick a user from the current room. Note: user is in the form nick#1234")
+    async def kick(self, ctx, user):
+        if not discord.utils.get(ctx.author.roles, name=ctx.channel.name):
+            await ctx.reply("You don't have permission to do this.")
+            return
+        if not '#' in user:
+            await ctx.reply("Since this channel's private you'll need to kick them as user#1234")
+            return
+        name, discriminator = user.split("#")
+        who = discord.utils.get(ctx.guild.members, name=name, discriminator=discriminator)
+        if not who:
+            await ctx.reply("This user doesn't exist in this server")
+            return
+        role = discord.utils.get(ctx.guild.roles, name=ctx.channel.name)
+        if not role:
+            await ctx.reply("This isn't a private enough channel!")
+            return
+        await who.remove_roles(role)
+        await ctx.message.add_reaction("👍")
+
+    @private.command(help="Leave the current room.")
+    async def leave(self, ctx):
+        role = discord.utils.get(ctx.guild.roles, name=ctx.channel.name)
+        if role:
+            await ctx.author.remove_roles(role)
         await ctx.message.add_reaction("👍")
 
 def setup(bot):
