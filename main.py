453a06afed07aa0aa7ed1228462a872c56d922d6
Tom Powell <tom@powell.io>
2021-10-18 23:58:05 -0700

    Make bot incredibly less helpful

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.members = True
 
+
+def has_any_role(ctx):
+    # is in guild, has author, and has some role besides @everyone
+    return ctx.guild and ctx.author and len(ctx.author.roles) > 1
+
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+help_command = discord.utils.get(bot.commands, name="help")
+help_command.add_check(has_any_role)
 
 for cog in os.listdir("./cogs"):
     if cog.endswith(".py"):
@@ -22,6 +30,14 @@ async def on_ready():
 
 @bot.event
 async def on_command_error(ctx, error):
-    await ctx.message.reply(f"error: {error}")
+    if not has_any_role(ctx):
+        await ctx.message.reply(f"No.")
+    else:
+        await ctx.message.reply(f"error: {error}")
+
+@bot.check
+async def globally_block_dms(ctx):
+    return ctx.guild is not None
+
 
 bot.run(os.getenv("DISCORD_BOT_TOKEN"))
