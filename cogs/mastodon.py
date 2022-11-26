4177cefc20b941fedf9e44f9004663eebf1ffe2d
Tom Powell <tom@powell.io>
2022-11-25 20:54:23 -0800

    move mastodon notifications to an embed

--- cogs/mastodon.py
+++ cogs/mastodon.py
@@ -1,11 +1,12 @@
 from discord.ext import tasks, commands
+from bs4 import BeautifulSoup
 
 import discord
 import os
 import requests
 
 
-class MastodonNotification(commands.Cog):
+class Mastodon(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.fetch_notifications.start()
@@ -14,14 +15,14 @@ class MastodonNotification(commands.Cog):
         }
 
     def cog_unload(self):
-        self.fetch_notifications.cancel()
+        self.fetch_mentions.cancel()
 
     @commands.Cog.listener()
     async def on_ready(self):
         print(f"Loaded {__name__}")
 
     @tasks.loop(seconds=30)
-    async def fetch_notifications(self):
+    async def fetch_mentions(self):
         if len(self.bot.guilds) == 0:
             return
 
@@ -30,7 +31,9 @@ class MastodonNotification(commands.Cog):
             if not channel:
                 print("Please !config hset config mastodon.channel #channel")
             channel_id = channel.decode("utf-8")[2:-1]
-            self.channel = discord.utils.get(self.bot.guilds[0].channels, id=int(channel_id))
+            self.channel = discord.utils.get(
+                self.bot.guilds[0].channels, id=int(channel_id)
+            )
 
         last_notif = self.redis.get("mastodon.last_notif")
         if last_notif:
@@ -47,13 +50,30 @@ class MastodonNotification(commands.Cog):
 
         if req.status_code == 200:
             for notif in req.json():
-                if not last_notif or notif['id'] > last_notif:
-                    last_notif = notif['id']
-                await self.channel.send(notif['status']['url'])
+                if not last_notif or notif["id"] > last_notif:
+                    last_notif = notif["id"]
+
+                soup = BeautifulSoup(notif["status"]["content"], features="html.parser")
+                embed = discord.Embed.from_dict(
+                    {
+                        "title": "New Mention",
+                        "type": "rich",
+                        "author": {
+                            "name": f"{notif['account']['display_name']} ({notif['account']['acct']})",
+                            "url": notif["account"]["url"],
+                        },
+                        "description": soup.get_text(),
+                        "thumbnail": {
+                            "url": notif["account"]["avatar"],
+                        },
+                        "url": notif["status"]["url"],
+                    }
+                )
+                await self.channel.send(content=None, embed=embed)
             self.redis.set("mastodon.last_notif", last_notif)
         else:
             print(req.status_code, req.text)
 
 
 def setup(bot):
-    bot.add_cog(MastodonNotification(bot))
+    bot.add_cog(Mastodon(bot))
