f551464327ecec38f9f5aa1c39e9b799bbeba97b
Tom Powell <tom@powell.io>
2023-07-03 21:44:19 -0700

    add echo

--- cogs/util.py
+++ cogs/util.py
@@ -12,6 +12,10 @@ class Util(commands.Cog):
     async def get_superuser_roles(self):
         return [x.decode("utf-8") for x in self.redis.smembers("superuser_roles")]
 
+    @commands.command(help="echo raw text")
+    async def echo(self, ctx):
+        await ctx.reply(f"```{ctx.message}```")
+
 
 async def setup(bot):
     await bot.add_cog(Util(bot))
