af23cdebb6593091d7ae919b98c2a3fc7279bd3f
Tom Powell <tom@powell.io>
2023-07-20 10:00:33 -0700

    return errors, use correct buildkite slug

--- cogs/buildkite.py
+++ cogs/buildkite.py
@@ -23,8 +23,10 @@ class Buildkite(commands.Cog):
         repo, branch = dest.split("#")
         src_repo, src_branch = src.split("#")
         data = {'branch': 'main', 'commit': 'HEAD', 'env': {'REPO': f'LineageOS/{repo}', 'DEST_BRANCH': branch, 'SRC_REPO': src_repo, 'SRC_BRANCH': src_branch}, 'message': f'forcepush of {repo} by {ctx.message.author.name}'}
-        resp = requests.post('https://buildkite.com/v2/organizations/LineageOS/pipelines/forcepush/builds', json=data, headers={"Authorization": f"Bearer {os.environ.get('BUILDKITE_TOKEN')"})
+        resp = requests.post('https://api.buildkite.com/v2/organizations/lineageos/pipelines/forcepush/builds', json=data, headers={"Authorization": f"Bearer {os.environ.get('BUILDKITE_TOKEN')}"})
         if resp.status_code == 201:
-            ctx.message.reply(f"started: {resp.json()['web_url']}")
+            await ctx.message.reply(f"started: {resp.json()['web_url']}")
+        else:
+            await ctx.message.reply(f'failed: ```{resp.text[:1500]}```')
 async def setup(bot):
     await bot.add_cog(Buildkite(bot))
