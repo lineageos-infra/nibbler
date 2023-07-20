9dc5a2e7e04a56c25e847526e139bb67e9bf4084
Tom Powell <tom@powell.io>
2023-07-20 09:36:42 -0700

    fixup

--- cogs/buildkite.py
+++ cogs/buildkite.py
@@ -22,7 +22,7 @@ class Buildkite(commands.Cog):
     async def forcepush(self, ctx, dest: str, src: str):
         repo, branch = dest.split("#")
         src_repo, src_branch = src.split("#")
-        data = {'branch': 'main', 'commit': 'HEAD', 'env': {'REPO': f'LineageOS/{repo}', 'DEST_BRANCH': branch, 'SRC_REPO': src_repo, 'SRC_BRANCH': src_branch}, message: f'forcepush of {repo} by {ctx.message.author.name}'}
+        data = {'branch': 'main', 'commit': 'HEAD', 'env': {'REPO': f'LineageOS/{repo}', 'DEST_BRANCH': branch, 'SRC_REPO': src_repo, 'SRC_BRANCH': src_branch}, 'message': f'forcepush of {repo} by {ctx.message.author.name}'}
         resp = requests.post('https://buildkite.com/v2/organizations/LineageOS/pipelines/forcepush/builds', json=data, headers={"Authorization", "Bearer {os.environ.get('BUILDKITE_TOKEN')"})
         if resp.status_code == 201:
             ctx.message.reply(f"started: {resp.json()['web_url']}")
