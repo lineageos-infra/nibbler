8eb31333754668d177874dd45dbcb9da0ec0b942
Tom Powell <tom@powell.io>
2023-07-20 09:31:27 -0700

    auth buildkite

---
+++ cogs/buildkite.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import tasks, commands
+import requests
+
+class Buildkite(commands.Cog):
+    '''Buildkite provides multiple funtions - this is where most of the permission related stuff for lineage lives.
+    - makes #roles operate, see !help buildkite
+    - quickly add people to the maintainer role via !maintainer (mention or user)
+    '''
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"Loaded {__name__}")
+
+    @commands.group(help="manage buildkite jobs")
+    @commands.has_role("Project Director")
+    async def buildkite(self, ctx):
+        pass
+
+    @buildkite.command(name="forcepush", help="force push a branch. example: hudson#main https://github.com/lineageos/hudson#main")
+    async def forcepush(self, ctx, dest: str, src: str):
+        repo, branch = dest.split("#")
+        src_repo, src_branch = src.split("#")
+        data = {'branch': 'main', 'commit': 'HEAD', 'env': {'REPO': f'LineageOS/{repo}', 'DEST_BRANCH': branch, 'SRC_REPO': src_repo, 'SRC_BRANCH': src_branch}, message: f'forcepush of {repo} by {ctx.message.author.name}'}
+        resp = requests.post('https://buildkite.com/v2/organizations/LineageOS/pipelines/forcepush/builds', json=data, headers={"Authorization", "Bearer {os.environ.get('BUILDKITE_TOKEN')"})
+        if resp.status_code == 201:
+            ctx.message.reply(f"started: {resp.json()['web_url']}")
