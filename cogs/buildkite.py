f2cf887465d567aaaa251ec28127e0968f0389a7
Tom Powell <tom@powell.io>
2023-08-11 21:26:31 -0700

    stop doing extra parsing

--- cogs/buildkite.py
+++ cogs/buildkite.py
@@ -69,10 +69,8 @@ class Buildkite(commands.Cog):
         else:
             await ctx.message.reply(f'failed: ```{resp.text[:1500]}```')
 
-    @buildkite.command(name="forcepush", help="force push a branch. example: hudson|main https://github.com/lineageos/hudson|main")
-    async def forcepush(self, ctx, dest: str, src: str):
-        repo, branch = dest.split("|")
-        src_repo, src_branch = src.split("|")
+    @buildkite.command(name="forcepush", help="force push a branch. example: hudson main https://github.com/lineageos/hudson main")
+    async def forcepush(self, ctx, repo: str, branch: str, src_repo: str, src_branch: str):
         data = {'branch': 'main', 'commit': 'HEAD', 'env': {'REPO': f'LineageOS/{repo}', 'DEST_BRANCH': branch, 'SRC_REPO': src_repo, 'SRC_BRANCH': src_branch}, 'message': f'forcepush of {repo} by {ctx.message.author.name}'}
         resp = requests.post('https://api.buildkite.com/v2/organizations/lineageos/pipelines/forcepush/builds', json=data, headers={"Authorization": f"Bearer {os.environ.get('BUILDKITE_TOKEN')}"})
         if resp.status_code == 201:
