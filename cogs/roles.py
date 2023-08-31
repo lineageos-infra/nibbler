16edba4947ea5a7b0910edbfdb33db493ecf333b
Tom Powell <tom@powell.io>
2023-08-31 14:32:08 -0700

    drop reactionroles, use onboarding

--- cogs/roles.py
+++ cogs/roles.py
@@ -4,7 +4,6 @@ from discord.ext import tasks, commands
 
 class Roles(commands.Cog):
     '''Roles provides multiple funtions - this is where most of the permission related stuff for lineage lives.
-    - makes #roles operate, see !help reactionrole
     - quickly add people to the maintainer role via !maintainer (mention or user)
     '''
     def __init__(self, bot):
@@ -17,112 +16,6 @@ class Roles(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print(f"Loaded {__name__}")
-        self.channel = discord.utils.get(self.bot.guilds[0].channels, name="roles")
-        await self.do_update()
-
-    @commands.Cog.listener()
-    async def on_raw_reaction_add(self, payload):
-        if self.bot.user.id == payload.member.id:
-            return
-        if payload.channel_id != self.channel.id:
-            return
-        # get role
-        emoji = payload.emoji.name
-        try:
-            role = discord.utils.get(payload.member.guild.roles, name=self.redis.hget("roles", emoji).decode('utf-8'))
-            if role:
-                if role in payload.member.roles:
-                    await payload.member.remove_roles(role, reason="clicked emoji")
-                else:
-                    await payload.member.add_roles(role, reason="clicked emoji")
-        except:
-            pass
-        message = await self.channel.fetch_message(payload.message_id)
-        await message.remove_reaction(emoji, payload.member)
-
-    @commands.group(help="reaction-role config")
-    @commands.is_owner()
-    async def reactionrole(self, ctx):
-        pass
-
-    @reactionrole.command(name="add", help="add a role for reaction-joining")
-    async def add_role(self, ctx, category: str, role: str, emoji: str, info: str):
-        if not role in [x.name for x in ctx.guild.roles]:
-            await ctx.guild.create_role(name=role, reason="auto created for role-reactions")
-        self.redis.hset("roles", emoji, role)
-        self.redis.hset("roleinfo", emoji, info)
-        self.redis.hset("rolecategories", emoji, category)
-        await ctx.message.add_reaction("👍")
-
-    @reactionrole.command(name="del", help="delete a role by name")
-    async def _del(self, ctx, emoji):
-        self.redis.hdel("roles", emoji)
-        self.redis.hdel("roleinfo", emoji)
-        self.redis.hdel("rolecategories", emoji)
-        await ctx.message.add_reaction("👍")
-
-    @reactionrole.command(help="list all the roles available for reactions")
-    async def get(self, ctx):
-        roles = self.redis.hgetall("roles")
-        info = self.redis.hgetall("roleinfo")
-        categories = self.redis.hgetall("rolecategories")
-        message = ""
-        for k, v in roles.items():
-            c = categories.get(k)
-            i = info.get(k)
-            message += f"{k.decode('utf-8')}: {v.decode('utf-8')} (info: {i}, category: {c})\n"
-
-
-        await ctx.send(message)
-        await ctx.message.add_reaction("👍")
-
-    async def do_update(self):
-        if len(self.bot.guilds) == 0:
-            return
-        if not hasattr(self, 'channel'):
-            self.channel = discord.utils.get(self.bot.guilds[0].channels, name="roles")
-
-        message = [x async for x in self.channel.history(limit=1)]
-        if message:
-            cmd = message[0].edit
-            await message[0].clear_reactions()
-        else:
-            cmd = self.channel.send
-
-        roles = self.redis.hgetall("roles")
-        info = self.redis.hgetall("roleinfo")
-        categories = self.redis.hgetall("rolecategories")
-
-        content = "Some channels require roles - react below:\n"
-
-        data = {}
-
-        for role in roles.keys():
-            if categories[role] not in data:
-                data[categories[role]] = []
-            data[categories[role]].append(f"{role.decode('utf-8')} {info.get(role).decode('utf-8')}")
-
-        for k,v in data.items():
-            content += f"**{k.decode('utf-8')}**\n".upper()
-            for i in v:
-                content += f"{i}\n"
-
-
-        m = await cmd(content=content)
-
-        # react to it with all the things
-        message = [x async for x in self.channel.history(limit=1)]
-        for k, v in roles.items():
-            await message[0].add_reaction(k.decode('utf-8'))
-
-    @reactionrole.command(help="trigger an update of the reaction-role message in #roles")
-    async def update(self, ctx):
-        await self.do_update()
-        await ctx.message.add_reaction("👍")
-
-    @tasks.loop(minutes=15)
-    async def update_task(self):
-        await self.do_update()
 
     @commands.command(help="Add maintainers via their global user name")
     @commands.has_role("Project Director")
